daa1e9dbd2da6492256579872c2943d809dfbb6b
KsRyY <andy200511@126.com>
2019-08-23 16:35:16 +0800

    (WIP) expressResponse object support

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 function convert(expressMiddleware) {
-	return async (ctx, next) => {
+	return async function convertedMiddleware(ctx, next) {
 		const expressSettingTable = {};
+		// Express's res.app and req.app implementation in Koa
+		const app = {
+			...ctx.app,
+			set(key, value) {
+				expressSettingTable[key] = value;
+			},
+			get(...args) {
+				if (args.length === 1) {
+					return expressSettingTable[args[0]];
+				}
+				return undefined;
+			},
+			// HTTP METHOD verb placeholder
+			post() {},
+			delete() {},
+			put() {},
+			patch() {},
+			all() {},
+			head() {},
+			checkout() {},
+			copy() {},
+			lock() {},
+			merge() {},
+			mkactivity() {},
+			mkcol() {},
+			move() {},
+			'm-search': () => {},
+			notify() {},
+			options() {},
+			purge() {},
+			report() {},
+			search() {},
+			subscribe() {},
+			trace() {},
+			unlock() {},
+			unsubscribe() {},
+		};
 		// Express's req object implementation on Koa
 		const expressReq = {
-			app: {
-				...ctx.app,
-				set(key, value) {
-					expressSettingTable[key] = value;
-				},
-				get(...args) {
-					if (args.length === 1) {
-						return expressSettingTable[args[0]];
-					}
-					return undefined;
-				},
-				// HTTP METHOD verb placeholder
-				post() {},
-				delete() {},
-				put() {},
-				patch() {},
-				all() {},
-				head() {},
-				checkout() {},
-				copy() {},
-				lock() {},
-				merge() {},
-				mkactivity() {},
-				mkcol() {},
-				move() {},
-				'm-search': () => {},
-				notify() {},
-				options() {},
-				purge() {},
-				report() {},
-				search() {},
-				subscribe() {},
-				trace() {},
-				unlock() {},
-				unsubscribe() {},
-			},
+			app,
 			baseUrl: ctx.url,
 			body: (ctx.request.body ? ctx.request.body : undefined),
 			fresh: ctx.fresh,
@@ -66,6 +68,40 @@ function convert(expressMiddleware) {
 			range() {},
 		};
 		// Express's response object implementation on Koa
-		let expressResponse = {};
+		const expressResponse = {
+			app,
+			headersSent: ctx.response.headerSent,
+			locals: {},
+			append: ctx.append,
+			attachment: ctx.attachment,
+			cookie: ctx.cookie.set,
+			clearCookie() {},
+			sendFile() {},
+			download() {},
+			end() {},
+			format() {},
+			get: ctx.response.get,
+			set: ctx.set,
+			json(body) {
+				ctx.body = body;
+			},
+			jsonp(body) {
+				ctx.body = body;
+			},
+			location() {},
+			links() {},
+			redirect: ctx.redirect,
+			render() {},
+			send(body) {
+				ctx.body = body;
+			},
+			sendStatus(status) {
+				ctx.status = status;
+			},
+			status(status) {
+				ctx.status = status;
+			},
+			vary: ctx.response.vary,
+		};
 	};
 }
